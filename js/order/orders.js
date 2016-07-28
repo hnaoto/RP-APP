6d13b655e1cdadb3d9587c0fa60e0bc0839699ce
hnaoto <hnaoto@me.com>
2016-07-28 06:57:00 -0400

    update profile

---
+++ js/order/orders.js
@@ -0,0 +1,23 @@
+
+'use strict';
+
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  NavigatorIOS,
+  TouchableHighlight,
+	TouchableOpacity,
+	AsyncStorage,
+} from 'react-native';
+
+
+export default class Orders extends Component {
+	
+
+
+}
